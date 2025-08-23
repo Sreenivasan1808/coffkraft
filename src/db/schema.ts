c5841887dc5b06e269df0cee8413c29af0ae3f79
Sreenivasan <sreeniezio18@gmail.com>
2025-08-23 10:30:28 +0530

    verification id made unique in db

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,7 +2,7 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const coffeeTable = sqliteTable("coffee_table", {
   id: int().primaryKey({ autoIncrement: true }),
-  verificationid: text().notNull(),
+  verificationid: text().notNull().unique(),
   department: text().notNull(),
   quantity: int().notNull(),
   cost: int().notNull(),
